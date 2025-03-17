99ea3057f0653eda5c7ffbc7c577536221a21cb2
Tanu Yadav <155564175+tanuyadav13@users.noreply.github.com>
2025-03-17 16:50:02 +0530

    Create NaNandString.js

---
+++ Basics1/NaNandString.js
@@ -0,0 +1,36 @@
+const firstName ='Hello'
+const lastName ="HC"
+
+console.log(firstName+ ' ' + lastName);
+console.log(`my firstname ${firstName} is and my lastname is ${lastName}`);
+
+
+const usesrname = new String('tanudotcom')
+console.log(username[0]);
+console.log(username.length); 
+console.log(username.charAt(0)); 
+console.log(username.substring(0,4)); 
+
+let email = "t@hc.com  "
+console.log(email.trim());
+let emailTwo = "Tw@pw.live"
+console.log(emailTwo.replace('Tw','Tanu'));
+
+console.log(email.include('hc'));
+
+
+
+
+//Numbers
+
+const num = new Number(5.3456)
+console.log(num.toFixed(2));
+
+
+const subscribers = 10000
+console.log(subscribers.tolocaleString());
+console.log(Numbers.MAX_VALUE);
+
+console.log(Math);
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10); //
