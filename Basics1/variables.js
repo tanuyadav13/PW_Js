69356a9d2f88fe1fa51f1122d62ed82c5e87ed49
Tanu Yadav <155564175+tanuyadav13@users.noreply.github.com>
2025-03-17 16:47:24 +0530

    Create variables.js

---
+++ Basics1/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+var accountemail = "xyz@gmmail.com"
+var accountPassword = "qwerty20"
+accountCity = "Bahraich"
+let accountState;
+
+
+
+//accountId = 2 //not alllowed, Number And Special Character are Not allowed for Variables Names.
+console.log(accountId);
+
+/*
+prefer not to use var be'coz of issue in block scope and functional scope
+*/
+
+accountPassword ="12343535"
+accountemail= "asdf@gmail.xom"
+accountCity= "Barabanki"
+console.table([accountPassword,accountemail,accountCity,accountId,accountState])
