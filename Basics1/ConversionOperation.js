f913c3ecfe368e966f7cadd3337d6da96110e73b
Tanu Yadav <155564175+tanuyadav13@users.noreply.github.com>
2025-03-17 16:49:17 +0530

    Create ConversionOperation.js

---
+++ Basics1/ConversionOperation.js
@@ -0,0 +1,85 @@
+// pw Skills Live Class
+
+let bankBalance ='100'
+let bankBalanceinInt  = parseInt(bankBalance)
+// console.log(bankBalance);
+// console.log(bankBalanceinInt);
+
+
+console.log( typeof bankBalance);
+ console.log(typeof bankBalanceinInt);
+
+//In boolean --> true ; empty value , 0 --> false;
+// NaN == NaN   False
+// 2 ==2 true
+// 5 + undefined  Nan
+// 5 = null  5
+
+
+//Self Study
+let score =true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abx" => NaN
+//true =>1; false=>0
+
+let isLoggedin = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log( booleanIsLoggedIn);
+
+// 1 =>true ; 0=> false
+//"" => false
+//"tanu"=> true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// ************** Operators *************
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 ="TAnu"
+let str3 = str1+ str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log( "1" + 2+2);
+// console.log( 2+2+ "1");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1 ,num2 ,num3
+
+num1 = num2 =num3 = 2+2;
+
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
